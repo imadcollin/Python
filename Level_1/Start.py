e04650036447ab5b2c94abc03b9e5c2fc5e89e96
Imad Collin <Admin@MacBook-Pro.local>
2017-11-04 19:44:22 +0100

    Print hello world

---
+++ Level_1/Start.py
@@ -0,0 +1,26 @@
+"""
+This file is first program with Python 
+"""
+print "Hello world "
+
+#If statments 
+if True:
+    print "True"
+else:
+    print "False"
+
+
+#If Else
+x=10
+if x<10: 
+    print "less"
+else:
+    print "greater"
+
+
+#If else in one
+if x<10:
+    print "less"
+elif x==10:
+    print "equal"
+    
