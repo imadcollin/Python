5e5206f8b8ab9cf2ce67eb75605e827219d70c2f
Imad Collin <Admin@MacBook-Pro.local>
2017-11-06 20:36:35 +0100

    Add first test

--- Testing/Test_1.py
+++ Testing/Test_1.py
@@ -12,17 +12,30 @@ None: Nothing to return
 
 import unittest 
 
+ ## Add pass function 
+def stringCheck(str):
+    pass
+
 class StringTest(unittest.TestCase):
     """Test for string length"""
 
+ 
+    def setUp(self):
+        """ Fixture that creates a string """
+        self.str="This is a string"
+
+    def tearDown(self):
+        """Fixture that remove the strings """
+        try:
+            self.str=""
+        except:
+            pass 
+
     def test_runner(self):
-        stringCheck()
+         stringCheck(self.str)
 
-## Add pass function 
-def stringCheck():
-    pass
-
-
+   
+    
 # Main 
 if __name__=="__main__":
     unittest.main()
