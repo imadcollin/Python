5c514b9e9d1c8340d2604f6864ec20f61c6f17a5
imadcollin <imad.collin@gmail.com>
2023-02-01 20:51:16 +0100

    Add monkey example

--- warmup_1/exe1.py
+++ warmup_1/exe1.py
@@ -1,22 +1,45 @@
-### Sleep in example 
+### Sleep in example
 def sleep_in(weekday, vacation):
-    if ( weekday & (not vacation)): 
-      return False; 
-    else: 
-      return True;
-
-def sleep_in_2(weekday, vacation): 
-    return (not weekday or vacation); 
+    if (weekday & (not vacation)):
+        return False
+    else:
+        return True
 
 
-assert sleep_in(True, True)==True;
-assert sleep_in(False, False)==True;
+def sleep_in_2(weekday, vacation):
+    return (not weekday or vacation)
+
+
+assert sleep_in(True, True) == True
 assert sleep_in(False, False) == True
-assert sleep_in(True, False) ==False
-assert sleep_in(False, True)== True
+assert sleep_in(False, False) == True
+assert sleep_in(True, False) == False
+assert sleep_in(False, True) == True
 
-assert sleep_in_2(True, True)==True;
-assert sleep_in_2(False, False)==True
+assert sleep_in_2(True, True) == True
 assert sleep_in_2(False, False) == True
-assert sleep_in_2(True, False) ==False
-assert sleep_in_2(False, True)== True
+assert sleep_in_2(False, False) == True
+assert sleep_in_2(True, False) == False
+assert sleep_in_2(False, True) == True
+
+
+def monkey_trouble(a, b):
+    return a == b
+
+
+def monkey_trouble2(a, b):
+    if (a and b):
+        return True
+    if (not a and not b):
+        return True
+    else:
+        return False
+
+
+assert monkey_trouble(True, True) == True
+assert monkey_trouble(False, False) == True
+assert monkey_trouble(True, False) == False
+
+assert monkey_trouble2(True, True) == True
+assert monkey_trouble2(False, False) == True
+assert monkey_trouble2(True, False) == False
