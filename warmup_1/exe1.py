23c9d0f7d20a17e502efc707a34ca8f524980ef2
imadcollin <imad.collin@gmail.com>
2023-02-02 22:47:46 +0100

    Add pos neg example

--- warmup_1/exe1.py
+++ warmup_1/exe1.py
@@ -43,3 +43,35 @@ assert monkey_trouble(True, False) == False
 assert monkey_trouble2(True, True) == True
 assert monkey_trouble2(False, False) == True
 assert monkey_trouble2(True, False) == False
+
+
+def parrot_trouble(talking, hour):
+    if(talking and (hour <7 or hour >20)): 
+        return True; 
+    else:
+        return False; 
+
+
+assert parrot_trouble(True, 6) == True
+assert parrot_trouble(True, 7) == False
+assert parrot_trouble(False, 6) == False
+
+
+def makes10(a, b):
+    return ((a == 10 or b==10) or (a+b==10))
+
+
+assert makes10(9, 10) == True
+assert makes10(9, 9) == False
+assert makes10(1, 9) == True
+
+
+def pos_neg(a, b, negative):
+    if(negative):
+        return (a<0 and b<0)
+    else:
+        return (a >0 and b < 0) or (a<0 and b>0)
+    
+assert pos_neg(-4, 5, True) == False
+assert pos_neg(-1, 1, False) == True
+assert pos_neg(-4, -5, True) == True	
