5e0ee64b24cf64c72af39b5b47807d7ac7c321a9
imadcollin <imad.collin@gmail.com>
2023-02-08 21:41:28 +0100

    Add string example

--- warmup_2/exe1.py
+++ warmup_2/exe1.py
@@ -20,3 +20,37 @@ def front_times(str, n):
 assert front_times('Chocolate', 2) == 'ChoCho'
 assert front_times('Chocolate', 3) == 'ChoChoCho'
 assert front_times('Abc', 3) == 'AbcAbcAbc'
+
+def string_bits(str):
+    s= ""
+    for i in range(len(str)):
+        if(i%2==0): 
+            s+=str[i];
+    return s; 
+
+assert string_bits('Hello') == 'Hlo'
+assert string_bits('Hi') =='H'
+assert string_bits('Heeololeo') == 'Hello'
+
+
+def string_splosion(str):
+    if(len(str) == 1): 
+        return str
+    if (len(str) ==2): 
+        return str[:1] + str
+    s= ""
+    for i in range (len(str)): 
+        s=s+ str[:i+1]; 
+    return s; 
+
+
+def string_splosion_2(str):
+    s = ""
+    for i in range (len(str)): 
+        s=s+ str[:i+1]; 
+    return s; 
+
+
+assert string_splosion_2('Code') == 'CCoCodCode'
+assert string_splosion_2('abc') == 'aababc'
+assert string_splosion_2('ab') =='aab'
