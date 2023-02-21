85c7fe2030455a588352a172422e048ae1a021f0
imadcollin <imad.collin@gmail.com>
2023-02-21 22:39:39 +0100

    Add devision example

--- Lists/list.py
+++ Lists/list.py
@@ -93,3 +93,14 @@ def sum2(nums):
 assert sum2([1, 2, 3]) == 3
 assert sum2([1, 1]) == 2
 assert sum2([1, 1, 1, 1]) ==2
+
+
+def middle_way(a, b):
+    arr = [0,0]  
+    arr[0] =a[1]
+    arr[1] =b[1]
+    return arr
+
+assert middle_way([1, 2, 3], [4, 5, 6]) == [2, 5]
+assert middle_way([7, 7, 7], [3, 8, 0]) == [7, 8]
+assert middle_way([5, 2, 9], [1, 4, 5]) == [2, 4]
---
+++ One100/easy.py
@@ -0,0 +1,17 @@
+import math 
+def divission(a,b):
+    return a//b
+
+
+assert divission(9,2) ==4 
+assert divission(17,5) ==3 
+assert divission(20,6) ==3 
+
+
+def divission_2(a,b):
+    return math.floor(a/b)
+
+
+assert divission_2(9,2) ==4 
+assert divission_2(17,5) ==3 
+assert divission_2(20,6) ==3 
