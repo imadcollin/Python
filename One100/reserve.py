8ab11f21897bb4f4ce728d3305f3cac62ecb1f5e
imadcollin <imad.collin@gmail.com>
2023-02-28 22:43:08 +0100

    Add first example

---
+++ One100/reserve.py
@@ -0,0 +1,32 @@
+def reverseInt(a):
+    x = ''
+    if (a <= 9):
+        return a
+    while (a > 0):
+        num = a % 10
+        x = x + str(num)
+        a = a // 10
+    return int(x)
+
+
+assert reverseInt(43) == 34
+assert reverseInt(435) == 534
+assert reverseInt(5765) == 5675
+
+
+def armstrong(a):
+    sum = 0
+    temp = a
+    if temp <= 9:
+        return a
+    while a > 0:
+        num = a % 10
+        cube = num * num * num
+        sum = sum + cube
+        temp = temp // 10
+    return sum == a
+
+
+assert armstrong(153) == True
+assert armstrong(370) == True
+assert armstrong(1333) == False
