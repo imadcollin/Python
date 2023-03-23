18fc6d671564751c7934e98f76a736eb207a0d70
imadcollin <imad.collin@gmail.com>
2023-03-23 22:00:08 +0100

    Add tuple example

---
+++ DS/tuple.py
@@ -0,0 +1,21 @@
+#Empty Tuple
+tuple = ()
+print(tuple)
+
+#Tuple
+tuple2 = (1,2,3,4,'test')
+tuple3 = 'test', 'test 2', 2,3
+
+print(tuple2)
+print(tuple3)
+
+#Access El
+print(tuple3[0])
+print(tuple3[1])
+print(tuple3[2])
+
+#Unpack
+tuple1 = 1,2,'test'
+a,b,c = tuple1
+print(a)
+print(b)
