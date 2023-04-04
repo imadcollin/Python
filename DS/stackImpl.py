cce8642ef13aa686471fcb46f6101dc6442c4684
imadcollin <imad.collin@gmail.com>
2023-04-04 20:58:11 +0200

    Add stack size method

--- DS/stackImpl.py
+++ DS/stackImpl.py
@@ -9,7 +9,13 @@ class Stack:
         else:
             print("Element Already Exist")
             return False
-        
+    
+    def peek(self): 
+        return self.stack.pop()
+    
+    def stackSize(self):
+        return len(self.stack)
+    
     def pritnStack(self): 
         for i in self.stack:
             print('elem -> ', i)
@@ -19,3 +25,8 @@ AStack =Stack()
 AStack.add('A') 
 AStack.add('B')
 AStack.pritnStack() 
+
+print('############ Peek #######33')
+AStack.peek()
+print(AStack.stackSize())
+AStack.pritnStack()
